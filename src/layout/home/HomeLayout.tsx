ed1f3139234e96d3867b924b50d445656ea685f3
MarGoN <monondcosta@gmail.com>
2025-05-14 17:08:58 +0600

    Modify: Navbar is now full width and outside Home.

--- src/layout/home/HomeLayout.tsx
+++ src/layout/home/HomeLayout.tsx
@@ -1,9 +1,11 @@
 //import React from 'react';
 import Home from '../../pages/home/Home';
 import Footer from '../../components/shared/Footer';
+import ResponsiveNavbar from '@/components/home/ResponsiveNavbar';
 const HomeLayout = () => {
     return (
         <div className='w-full space-y-16'>
+            <ResponsiveNavbar />
             <Home/>
             <Footer/>
         </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,24 +1,23 @@
+import Adv from "@/components/home/Adv";
+import NewsletterForm from "@/components/home/Newsletter";
 import Testimonial from "@/components/home/Testimonial";
 import Banner from "../../components/home/Banner";
 import Hero from "../../components/home/Hero";
-import ResponsiveNavbar from "../../components/home/ResponsiveNavbar";
 import FeaturedBicycles from "./FeaturedBicycles";
-import Adv from "@/components/home/Adv";
-import NewsletterForm from "@/components/home/Newsletter";
 import OurPartners from "./partners/OurPartners";
 
 const Home = () => {
   return (
     <div className="container mx-auto min-h-screen space-y-6 sm:space-y-8 lg:space-y-12 px-4 sm:px-6 lg:px-8">
-      <ResponsiveNavbar />
+      {/* <ResponsiveNavbar /> */}
       <div className="min-h-[30vh] sm:min-h-[40vh] my-6 sm:my-8 flex flex-col justify-center items-center gap-8 sm:gap-12 lg:gap-16">
         <Hero />
         <Banner />
         <FeaturedBicycles />
-        <Adv/>
-        <NewsletterForm/>
-        <Testimonial/>
-        <OurPartners/>
+        <Adv />
+        <NewsletterForm />
+        <Testimonial />
+        <OurPartners />
       </div>
     </div>
   );
